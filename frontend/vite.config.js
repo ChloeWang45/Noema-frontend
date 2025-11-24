793a27c5dfba76c44cebddb18adab4c146e4eee4
ChloeWang45 <67757174+ChloeWang45@users.noreply.github.com>
2025-11-24 01:03:19 -0500

    Fixed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Noema-frontend/",
   plugins: [react()],
-})
+  base: "/Noema-frontend/",   // GitHub Pages folder for your repo
+});
